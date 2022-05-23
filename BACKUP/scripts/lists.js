c03185a588d3cbdc374a57c6d481ea0164516078
e4494s <61923287+e4494s@users.noreply.github.com>
2022-05-22 19:48:28 -0500

    Create lists.js

---
+++ BACKUP/scripts/lists.js
@@ -0,0 +1,17 @@
+let names = [
+  "Gary",
+  "Bob",
+  "Fred",
+  "Jacob",
+  "Sam",
+  "Ralph",
+  "Bill",
+  "Tom",
+  "Jerry",
+  "Leroy",
+  "Josh"
+];
+
+function randIndex(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
